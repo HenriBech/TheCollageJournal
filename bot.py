f4cd8e3f72ab7912f9bb1241bdf5397833cd5f28
HenriBech <32742444+HenriBech@users.noreply.github.com>
2020-04-28 14:28:34 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,37 +1,56 @@
+# IMPORTS
 import tweepy as tp
 import time
 import datetime as dt
 from scraper import Archive
 from PIL import Image
 
-#CREDENTIALS
+# TWITTER API CREDENTIALS (do not commit to GitHub!)
 consumer_key = "consumer_key"
 consumer_secret = "consumer_secret"
 access_token = "access_token"
 access_secret = "access_secret"
 
-#LOGIN
+# TWITTER LOGIN
 auth = tp.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tp.API(auth)
 
-delay = 3600
+delay = 3600 													# delay (in s) before next post
 
-arch = Archive(dt.date(2009, 10, 30))
-arch.compile()
-arch.compilePosts()
-images = arch.compileImages()
+# INITIALIZATION
+arch = Archive(dt.date(2009, 11, 1))							# initialize archive
+arch.compile()													# compile initial archive
+arch.compilePosts()												# compile initial batch of posts
+images = arch.genImagePosts()									# create generator object to generate posts
+
+# BOT LOOP
 while True:
-	new_posts = arch.update()
+	new_posts = arch.update()									# check for new posts
 	if new_posts:
-		arch.compilePosts()
-		images = arch.compileImages()
-	else:
-	(date, text) = next(images, (None, None))
-	if date is None:
+		arch.compilePosts()										# compile new posting list
+		images = arch.genImagePosts()							# restart post generator
+	(date, caption) = next(images, (None, None))				# generate post; at StopIteration, the generator yields None
+	if date is None:											#  in this case the loop will wait and skip posting until new posts are created
 		time.sleep(delay)
 		continue
-	response = api.media_upload(f"./images/{date}.jpg")
-	api.update_status(status=text, media_ids=[response.media_id_string])
-	next(images)
-	time.sleep(delay)
+	elif caption.startswith("RT: "):							# check for retweet indicator
+		search = f"\"{date}\" (from:collage_journal) filter:links"
+		search_results = api.search(search, result_type=recent)	# if post has been made before, twitter search for the respective post
+		n = len(search_results)									# number of posts for that date
+		for result in search_result:
+			if not result.retweeted:
+				api.rt(result)									# retweet found posts which are not retweets
+				time.sleep(int(delay/n))						# wait shortened s/t all RTs of the same day occur within the usual delay
+		continue
+	image = Image.open(f"./images/{date}.jpg")
+	image.show()
+	print(caption)
+	try:														# try to post the downloaded image with the generated caption to twitter
+		response = api.media_upload(f"./images/{date}.jpg")		# upload media
+		api.update_status(status=caption, media_ids=[response.media_id_string])
+		arch.dir[date].posted = True							# indicate that the entry has now been posted
+	except:
+		print(f"Warning: failed to post to twitter at {dt.datetime.now()}")
+	next(images)												# control flow call to trigger the deletion of the downloaded image
+	time.sleep(delay)											# wait
--- scraper.py
+++ scraper.py
@@ -6,140 +6,159 @@ import os
 from bs4 import BeautifulSoup
 from dateutil.relativedelta import relativedelta
 
+# Class to store entry info
 class ArchiveEntry():
 	def __init__(self, date):
-		self.date = date
-		self.day = (self.date-dt.date(2005, 4, 30)).days
-		self.links = {}
+		self.date = date 									# date of the entry, same as the key in parent dictionary
+		self.day = (self.date-dt.date(2005, 4, 30)).days 	# days since 2005-4-30, the start of the Collage Journal
+		self.links = {} 									# dictionary that stores (title, link) as (key, value) pair
+		self.posted = False									# bool to check if the post has been made before
+		pass
 
+# Class for the archive of posts with the various methods to fetch and compile images and posts
 class Archive():
-	#initialization
+	# Initialization
 	def __init__(self, start_day):
-		self.journal_start = start_day
-		self.dir = {}
-		self.day = dt.date.today()
-		self.posts = []
+		self.journal_start = start_day 	# class is created with the date of the first post that's archived
+		self.dir = {}					# the archive, which stores (date, ArchiveEntry) as (key, value) pair
+		self.day = dt.date.today()		# current day
+		self.posts = []					# list of twitter posts to make, updated daily
 		print(f"Archive intialized on {self.day} with start at {self.journal_start}")
 		pass
 
+	# Method to add entry to the archive dictionary, 
+	#  returns boolean to indicate whether a new ArchiveEntry was created
 	def addEntry(self, date, title, link):
 		if date in self.dir:
-			self.dir[date].links[title] = link
+			self.dir[date].links[title] = link 	# add new (title, link) entry to exsiting ArchiveEntry.links dictionary
 			return False
 		else:
 			new_entry = ArchiveEntry(date)
 			new_entry.links[title] = link
-			self.dir[date] = new_entry
+			self.dir[date] = new_entry			# add newly created ArchiveEntry to archive
 			return True
 
+	# Function that returns the number of months since the the first archive entry
 	def nMonth(self):
 		return (self.day.year-self.journal_start.year)*12+(self.day.month-self.journal_start.month)
 
+	# Function that returns the number of days since the start of the Collage Journal (2005-4-30)
 	def daysSince(self):
 		return (self.day-dt.date(2005, 4, 30)).days
 
-	def dateFromTitle(self, title):
-		date = [i for i in title.split() if '/' in i]
-		date = date[0].split('/')
-		date[2] = re.sub("[^0-9]", "", date[2])
-		year = int(date[2] if len(date[2])==4 else "20"+date[2])
-		month = int(date[0])
-		day = int(date[1])
-		return dt.date(year, month, day)
-
+	# Method that compiles the initial archive,
+	#  ideally only called once, subsequent entries to be added using self.update()
 	def compile(self, warnings=False):
-		self.day = dt.date.today()
+		self.day = dt.date.today()													# update date
 		y, m = self.journal_start.year, self.journal_start.month
-		tcj_archive = []
+		month_links = []
+		# create list of links that refer to the monthly overviews of the Collage Journal website
 		for i in range(self.nMonth()):
-			tcj_archive.append(f"http://thecollagejournal.blogspot.com/{y}/{m:02d}/")
+			month_links.append(f"http://thecollagejournal.blogspot.com/{y}/{m:02d}/")
 			m += 1
 			if m>12:
 				m=1
 				y += 1
-		for month in tcj_archive:
-			soup = BeautifulSoup(requests.get(month).text, 'html.parser')
-			for post in soup.find_all("div", class_="post hentry")[::-1]:
-				title = post.find("h3")
+		# gather links of all posts per month
+		for month in month_links:
+			soup = BeautifulSoup(requests.get(month).text, 'html.parser')			# grab HTML
+			for post in soup.find_all("div", class_="post hentry")[::-1]: 			# individual posts are indicated by the "post hentry" class tag
+				title = post.find("h3")												# get post title
+				published = post.find("abbr", class_="published")["title"]			# get timestamp from "published" tag
+				dates = [int(val) for val in published.split('T')[0].split('-')]	# format date
+				date = dt.date(*dates)
 				try:
-					date = self.dateFromTitle(title.contents[1].contents[0])
+					link = title.a["href"]											# get post link
+					self.addEntry(date, title.contents[1].contents[0], link)		# add archive entry for new link
 				except:
 					if warnings:
-						print(f"Warning: unparseable date on {prev_date-dt.timedelta(days=1)}")
-					continue
-				try:
-					link = title.a["href"]
-					self.addEntry(date, title.contents[1].contents[0], link)
-				except:
-					if warnings:
-						print(f"Warning: irregular HTML on {date}")
+						print(f"Warning: entry could not be added on {date}")
 					pass
-				prev_date = date
 		pass
 
+	# Method to update archive with new posts from the frontpage of the Collage Journal blog
+	#  returns list of dates of newly created entries
 	def update(self, warnings=False):
-		soup = BeautifulSoup(requests.get("http://thecollagejournal.blogspot.com/").text, 'html.parser')
-		self.day = dt.date.today()
-		new_posts = []
-		for post in soup.find_all("div", class_="post hentry"):
-				title = post.find("h3")
-				try:
-					date = self.dateFromTitle(title.contents[1].contents[0])
-				except:
-					if warnings:
-						print(f"Warning: unparseable date")
-					continue
-				link = title.a["href"]
-				if self.addEntry(date, title.contents[1].contents[0], link):
-					new_posts.append(date)
+		self.day = dt.date.today()																		# update date
+		soup = BeautifulSoup(requests.get("http://thecollagejournal.blogspot.com/").text, 'html.parser')# grab HTML
+		new_posts = []																					# list of new entries
+		# gather links on frontpage
+		for post in soup.find_all("div", class_="post hentry"):											# individual posts are indicated by the "post hentry" class tag
+			title = post.find("h3")																		# get post title
+			published = post.find("abbr", class_="published")["title"]									# get timestamp from "published" tag
+			date = dt.date(*[int(val) for val in published.split('T')[0].split('-')])					# format date
+			link = title.a["href"]																		# get post link
+			if self.addEntry(date, title.contents[1].contents[0], link):								# add archive entry for new link, if link already exist the addEntry method makes no changes
+				new_posts.append(date)																	# if new, add to new entries
 		return new_posts
 
+	# Method to compile the list of daily twitter posts
+	#  gathers all previous posts of the same date
 	def compilePosts(self):
+		self.posts = []										# empty post list
 		date=self.day
-		step = (4 if date.month==2 and date.day==29 else 1)
+		step = (4 if date.month==2 and date.day==29 else 1)	# exception for leap years
 		while date>=self.journal_start:
-			self.posts.append(date)
-			date = date - relativedelta(years=step)
+			self.posts.append(date)							# add post
+			date = date - relativedelta(years=step)			# move back a year
 		pass
 
-	def imageScrape(self, link): #hacky af
-		soup = BeautifulSoup(requests.get(link).text, 'html.parser')
-		image = soup.find("div", class_="post-body entry-content").a["href"]
-		request = requests.get(image)
-		if 's1600-h' in image: #ipretendidonotseeit.jpeg
-			try:
-				deep_soup = BeautifulSoup(request.text, 'html.parser')
-				deep_image = deep_soup.body.img["src"]
-				re = deep_image
-			except:
-				re = image
+	# Function to grab the image link from a given post link (somewhat static due to string checking)
+	def imageScrape(self, link):
+		soup = BeautifulSoup(requests.get(link).text, 'html.parser')		# grab HTML
+		image = soup.find("div", class_="post-body entry-content").a["href"]# get image link
+		request = requests.get(image)										# request image
+		# check for link tags:
+		#  's1600' indicates live image link
+		#  's1600-h' indicates irregular embedding
+		if 's1600' in image:
+			if 's1600-h' in image:
+				try: 														# try to access the irregular embedding
+					embedded = BeautifulSoup(request.text, 'html.parser')	# grab embedded HTML
+					embedded_image = embedded.body.img["src"] 				# get new image link
+					image_link = embedded_image
+				except:
+					image_link = None										# if irregular embedding could not be accessed, return None
+			else:
+				image_link = image
+			return image_link
 		else:
-			re = image
-		return re
+			return None														# if dead image link, return None
 
+	# Method to download an image specified by a given (link, date) pair
+	#  returns boolean do indicate successful download
 	def dlLink(self, link, date):
-		try:
-			image = self.imageScrape(link)
-			if 'show_photo' in image:
-				print(f"Image not available: {date}")
-				return False
+		image = self.imageScrape(link)				# get image link
+		if not image:
+			print(f"Image not available: {date}")
+			return False							# indicate failure to grab image link
+		try:										# try to download scraped link
+			path = f"./images/{date}.jpg"			# specify download path
+			with open(path, 'wb') as f:
+				f.write(requests.get(image).content)# write new image
+			return True								# indicate successful download
 		except:
-			print(f"Image could not be saved: {date}")
-			return False
-		path = f"./images/{date}.jpg"
-		with open(path, 'wb') as f:
-			f.write(requests.get(image).content)
-		return True
+			print(f"Download failed: {date}")
+			return False							# indicate failed download
 
-	def compileImages(self):
-		for date in self.posts:
-			entry = self.dir[date]
-			for title in entry.links:
-				link = entry.links[title]
-				if not self.dlLink(link, date):
+	# Generator to sequentially download images, specified by the self.posts list
+	#  yields the post text and deletes the downloaded image before the next download
+	def genImagePosts(self):
+		for date in self.posts:							# iterate over posts
+			if date in self.dir:
+				entry = self.dir[date]					# get entry of given date, if entry exists
+			else:
+				yield (None, None)						# yield similar to StopIteration in outer control flow
+				continue
+			for title in entry.links:					# iterate over (title, link) pairs in the dictionary
+				link = entry.links[title]				# get post link
+				post_text = f"{date} (Day {entry.day}) visit @ {link}"
+				if entry.posted:						# if post has been made before,
+					yield (date, "RT: "+post_text)		#  yield with retweet indicator
 					continue
+				if not self.dlLink(link, date):			# try to download image at link
+					continue							#  if download failed, move on to next post
 				else:
-					post_text = f"{date} (Day {entry.day}) visit @ {link}"
-					yield (date, post_text)
-					os.remove(f"./images/{date}.jpg")
-					yield None
+					yield (date, post_text)				# yield tuple of date and post text and  
+					os.remove(f"./images/{date}.jpg")	# delete downloaded image
+					yield None							# empty yield to trigger deletion
